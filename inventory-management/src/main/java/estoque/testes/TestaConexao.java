ebdb6473701d94980c8c3401226134059d2a4135
Arthur <arthurbds221@gmail.com>
2025-11-18 23:11:46 -0300

    Adicionada dependencia do  banco de dados e classe de testes

---
+++ inventory-management/src/main/java/estoque/testes/TestaConexao.java
@@ -0,0 +1,22 @@
+package estoque.testes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestaConexao {
+    public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/estoque_inventario";
+            String user = "root";
+            String password = "1234";
+
+            Connection conexao = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Conectado ao MySQL ");
+            conexao.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
